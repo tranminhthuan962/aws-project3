f81c36d75235d68723b3bce8cbb60166fa37cc85
tranminhthuan962 <68406735+tranminhthuan962@users.noreply.github.com>
2024-12-13 08:46:45 +0700

    update folder name

---
+++ app/config.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db_username = os.environ.get("DB_USERNAME", "c4user")
+db_password = os.environ.get("DB_PASSWORD", "mypassword")
+db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME", "prj3db")
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db = SQLAlchemy(app)
+
+app.logger.setLevel(logging.DEBUG)
